b1479b630210ad7966d3004cc2a0478ee90f3f21
Martin Woodward <martinwoodward@github.com>
2023-03-13 13:42:35 +0000

    Add case to 20

--- PrimeTestCase.py
+++ PrimeTestCase.py
@@ -3,8 +3,8 @@ import unittest
 class PrimeTestCase(unittest.TestCase):
     
     def test_true_if_is_prime(self):
-        # Test true for all valid primes between 1 and 10
-        primes = [2]
+        # Test true for all valid primes between 1 and 20
+        primes = [2, 3, 5, 7, 11, 13, 17, 19]
         for prime in primes:
             self.assertTrue(is_prime(prime))
 
