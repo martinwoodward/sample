968de699ef577bd9ca2ebd95f3214edb99a64933
Martin Woodward <martinwoodward@github.com>
2023-03-13 13:32:32 +0000

    Add Java example

---
+++ IsPrimeTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class IsPrimeTest {
+
+    /**
+     * Math.isPrime(int) returns wether the given number is prime or not
+     * Test valid primes from 1 to 10
+    **/ 
+    @Test
+    public void testTrueIfPrime() {
+    }
+    
+}
