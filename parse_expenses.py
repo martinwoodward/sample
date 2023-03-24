574e3e3d2b4d483cc55422606e8bebce4f3621a4
Martin Woodward <martinwoodward@github.com>
2023-03-24 00:20:38 +0000

    Format comments

--- parse_expenses.py
+++ parse_expenses.py
@@ -1,7 +1,8 @@
 import datetime
 
 def parse_expenses (expenses_string) :
-    """Parse the list of expenses and return the list of triples (date, value, currency).
+    """Parse the list of expenses and return the 
+    list of triples (date, value, currency).
     Ignore lines starting with #.
     Parse the date using datetime.
     Example expenses_string:
